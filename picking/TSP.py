bd0e2b4302674ee9d4fe49547e6ff1d66566288e
KyleKatann <48338675+KyleKatann@users.noreply.github.com>
2020-05-20 23:15:52 +0900

    fix tsp

--- picking/TSP.py
+++ picking/TSP.py
@@ -1,52 +1,31 @@
-#!/usr/bin/env python3.7
-
-# Copyright 2020, Gurobi Optimization, LLC
-
-# Solve a traveling salesman problem on a randomly generated set of
-# points using lazy constraints.   The base MIP model only includes
-# 'degree-2' constraints, requiring each node to have exactly
-# two incident edges.  Solutions to this model may contain subtours -
-# tours that don't visit every city.  The lazy constraint callback
-# adds new constraints to cut them off.
-
 import sys
-
 from itertools import combinations
 import gurobipy as gp
 from gurobipy import GRB
 
 n=0
 
-# Callback - use lazy constraints to eliminate sub-tours
 def subtourelim(model, where):
     if where == GRB.Callback.MIPSOL:
-        # make a list of edges selected in the solution
         vals = model.cbGetSolution(model._vars)
-        selected = gp.tuplelist((i, j) for i, j in model._vars.keys()
-                                if vals[i, j] > 0.5)
-        # find the shortest cycle in the selected edge list
+        selected = gp.tuplelist((i, j) for i, j in model._vars.keys() if vals[i, j] > 0.5)
         tour = subtour(selected)
         if len(tour) < n:
-            # add subtour elimination constr. for every pair of cities in tour
-            model.cbLazy(gp.quicksum(model._vars[i, j]
-                                     for i, j in combinations(tour, 2))
-                         <= len(tour)-1)
+            model.cbLazy(gp.quicksum(model._vars[i, j] for i, j in combinations(tour, 2)) <= len(tour)-1)
 
 
-# Given a tuplelist of edges, find the shortest subtour
 
 def subtour(edges):
     unvisited = list(range(n))
-    cycle = range(n+1)  # initial length has 1 more city
-    while unvisited:  # true if list is non-empty
+    cycle = range(n+1)
+    while unvisited:
         thiscycle = []
         neighbors = unvisited
         while neighbors:
             current = neighbors[0]
             thiscycle.append(current)
             unvisited.remove(current)
-            neighbors = [j for i, j in edges.select(current, '*')
-                         if j in unvisited]
+            neighbors = [j for i, j in edges.select(current, '*') if j in unvisited]
         if len(cycle) > len(thiscycle):
             cycle = thiscycle
     return cycle
@@ -62,9 +41,6 @@ def main(N,dist):
         vars[j, i] = vars[i, j]
 
     m.addConstrs(vars.sum(i, '*') == 2 for i in range(n))
-
-    # Optimize model
-
     m._vars = vars
     m.Params.lazyConstraints = 1
     m.optimize(subtourelim)
